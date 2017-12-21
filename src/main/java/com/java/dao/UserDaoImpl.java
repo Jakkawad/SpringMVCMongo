861651dee98dfda2f8640924ffe795efba24b1f0
Jakkawad Chaiplee <sunnysunoc@gmail.com>
2017-12-21 16:23:27 +0700

    master
    master

--- src/main/java/com/java/dao/UserDaoImpl.java
+++ src/main/java/com/java/dao/UserDaoImpl.java
@@ -1,8 +1,14 @@
 package com.java.dao;
 
+import com.java.config.WebConfig;
 import com.java.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,9 +20,21 @@ public class UserDaoImpl implements UserDao {
     @Autowired
     MongoTemplate mongoTemplate;
 
+//    ApplicationContext ctx = new AnnotationConfigApplicationContext(WebConfig.class);
+//    MongoOperations mongoOperations = (MongoOperations) ctx.getBean("mongoTemplate");
+
     private static final String COLLECTION_NAME = "user";
+
     public List<User> listUser() {
-        return mongoTemplate.findAll(User.class, COLLECTION_NAME);
+        BasicQuery query1 = (BasicQuery) new BasicQuery("{  }").limit(2);
+        Query query2 = new Query();
+//        return mongoTemplate.findAll(User.class, COLLECTION_NAME);
+//        User userTest = mongoOperations.findOne(query1, User.class);
+//        System.out.println(userTest);
+//        return mongoTemplate.findAll(User.class, COLLECTION_NAME);
+//        return mongoTemplate.findAll(User.class);
+        return mongoTemplate.find(query1, User.class);
+//        return mongoOperations.findOne(query1, User.class);
     }
 
     public void add(User user) {
--- src/main/java/com/java/model/User.java
+++ src/main/java/com/java/model/User.java
@@ -3,7 +3,7 @@ package com.java.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "user(")
 public class User {
 
     @Id
